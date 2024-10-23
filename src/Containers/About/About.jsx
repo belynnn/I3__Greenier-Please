bc72f9af71a2f753c577f622c15cb7382108366f
Sounia <s.mhalla@interface3.be>
2024-10-23 18:57:08 +0200

    Styling Home & About pages

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -1,8 +1,42 @@
+import styles from "./About.module.css";
+
 const About = () => {
 
     return (
         <>
-            <p>Page About</p>
+            <h2 className={styles.subtitle}>About</h2>
+            <h2 className={styles.subjectname}>Team</h2>
+
+            <section className={styles.firstsection}>
+                <div className={styles.membercards}>
+                    <h3>Luna</h3>
+                    <p>IMAGE/DESSIN/BLIBLOU</p>
+                </div>
+                <div className={styles.membercards}>
+                    <h3>Alex</h3>
+                    <p>IMAGE/DESSIN/BLIBLOU</p>
+                </div>
+                <div className={styles.membercards}>
+                    <h3>Gweny</h3>
+                    <p>IMAGE/DESSIN/BLIBLOU</p>
+                </div>
+                <div className={styles.membercards}>
+                    <h3>Béné</h3>
+                    <p>IMAGE/DESSIN/BLIBLOU</p>
+                </div>
+                <div className={styles.membercards}>
+                    <h3>Debby</h3>
+                    <p>IMAGE/DESSIN/BLIBLOU</p>
+                </div>
+                <div className={styles.membercards}>
+                    <h3>Audrey</h3>
+                    <p>IMAGE/DESSIN/BLIBLOU</p>
+                </div>
+                <div className={styles.membercards}>
+                    <h3>Audrey</h3>
+                    <p>IMAGE/DESSIN/BLIBLOU</p>
+                </div>
+            </section>
         </>
     );
 };
--- src/Containers/Index/Index.jsx
+++ src/Containers/Index/Index.jsx
@@ -1,8 +1,22 @@
+import styles from "./Index.module.css";
+
 const Index = () => {
 
     return (
         <>
-            <h1>Greenier, Please</h1>
+            <section className={styles.firstsection}>
+
+                <h1 className={styles.title}>Greenier, Please</h1>
+
+                <div className={styles.explanation}>
+                    <p>
+                        The player takes on the role of a project manager and receives 1 random contributor from each IT sector. Each participant will have 3 proposals, only 1 of which is “green”. Some proposals are eco-responsible (green) where others are only superficially (red). At any time, one or other of the speakers can spread false information to achieve his or her ends. Every decision taken by the person in charge of projects has an impact on the general mood of the company, filling the interface with rust and raising a thermostat if the carbon footprint explodes.
+                    </p>
+                </div>
+                <button className={styles.btnplay}>
+                    <img src="/assets/PlayButton.png" alt="Button Play Game" />
+                </button>
+            </section>
         </>
     );
 };
