4db9ad96423525c2019c0ed4ecd86356b3ec404a
Bénédicte Lagasse <benedicte.lagasse@proton.me>
2024-10-23 16:38:01 +0200

    added Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <ul>
-            <li> <Link to="">Home </Link></li>
-            <li> <Link to="/game">Game</Link> </li>
-            <li> <Link to="/about">About us</Link> </li>
-        </ul>
-    );
+  const location = useLocation();
+  return (
+    <ul>
+      {location.pathname !== '/' && (
+        <li>
+          <Link to=''>
+            <img src='' alt='Home page img' />
+            <p>Home</p>
+          </Link>
+        </li>
+      )}
+      {location.pathname !== '/game' && (
+        <li hidden={false}>
+          <Link to='/game'>
+            <img src='' alt='Game page img' />
+            <p>Game</p>
+          </Link>
+        </li>
+      )}
+      {location.pathname !== '/about' && (
+        <li hidden={false}>
+          <Link to='/about'>
+            <img src='' alt='About us page img' />
+            <p>About us</p>
+          </Link>
+        </li>
+      )}
+    </ul>
+  );
 };
 export default Navbar;
