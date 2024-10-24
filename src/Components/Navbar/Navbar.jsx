23d2f3015c1f3869b343bc98761e1aef40ac559b
Sounia <s.mhalla@interface3.be>
2024-10-24 18:55:50 +0200

    Added Style About page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       {location.pathname !== '/' && (
         <li>
           <Link to=''>
-            <img src='' alt='Home page img' />
+            <img src='/src/assets/Icons/house-line.svg' alt='Home page img' />
             <p>Home</p>
           </Link>
         </li>
@@ -16,15 +16,15 @@ const Navbar = () => {
       {location.pathname !== '/game' && (
         <li hidden={false}>
           <Link to='/game'>
-            <img src='' alt='Game page img' />
+            <img src='/src/assets/Icons/game-controller.svg' alt='Game page img' />
             <p>Game</p>
           </Link>
         </li>
       )}
       {location.pathname !== '/about' && (
         <li hidden={false}>
-          <Link to='/about'>
-            <img src='' alt='About us page img' />
+          <Link className={style.links} to='/about'>
+            <img src='/src/assets/Icons/info.svg' alt='About us page img' />
             <p>About us</p>
           </Link>
         </li>
--- src/Containers/Game/Game.jsx
+++ src/Containers/Game/Game.jsx
@@ -23,6 +23,9 @@ const Game = () => {
     <>
       <div className={styles.divgame}></div>
       <section className={styles.infosection}>
+        <div className={styles.information}>
+          <p>Infos des projets</p>
+        </div>
         <DivInfo />
       </section>
     </>
