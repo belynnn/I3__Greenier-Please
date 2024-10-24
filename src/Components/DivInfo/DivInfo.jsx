e14206f59062c1ea53983720fd490dfd8deb7911
Bénédicte Lagasse <benedicte.lagasse@proton.me>
2024-10-24 11:21:44 +0200

    added component DivInfo

---
+++ src/Components/DivInfo/DivInfo.jsx
@@ -0,0 +1,10 @@
+const DivInfo = () => {
+  return (
+    <>
+      <div>
+        <p>hello world</p>
+      </div>
+    </>
+  );
+};
+export default DivInfo;
--- src/Containers/Game/Game.jsx
+++ src/Containers/Game/Game.jsx
@@ -1,21 +1,19 @@
-import styles from "./Game.module.css";
+import DivInfo from '../../Components/DivInfo/DivInfo';
+import styles from './Game.module.css';
 
 const Game = () => {
+  return (
+    <>
+      <div className={styles.divgame}></div>
 
-    return (
-        <>
-            <div className={styles.divgame}>
-
-            </div>
-
-            <section className={styles.infosection}>
-                <div className={styles.information}>
-                    <p>Infos des projets</p>
-                </div>
-
-            </section>
-        </>
-    );
+      <section className={styles.infosection}>
+        <div className={styles.information}>
+          <p>Infos des projets</p>
+        </div>
+        <DivInfo />
+      </section>
+    </>
+  );
 };
 
 export default Game;
