9b5ef2af3e6bb2b736e7cfcc42f9cd9011e7c82b
Sounia <s.mhalla@interface3.be>
2024-10-24 13:59:38 +0200

    Script useEffect

--- src/Containers/Game/Game.jsx
+++ src/Containers/Game/Game.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react';
 import DivInfo from '../../Components/DivInfo/DivInfo';
 import styles from './Game.module.css';
+import { useLocation } from 'react-router-dom';
 
 const Game = () => {
+  // const location = useLocation();
+  // const [display, setDisplay] = useState(false);
+  // const handleLocation = () => {
+
+  //   if (location.pathname == "/game") {
+  //     () => (
+  //       setDisplay = !display
+
+  //     );
+
+  //   }
+  // };
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/GAME/game.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+
+
   return (
     <>
       <div className={styles.divgame}></div>
-
       <section className={styles.infosection}>
         <div className={styles.information}>
           <p>Infos des projets</p>
