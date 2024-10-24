0b876d247a185e44e2b31a630730f7a130010318
Bénédicte Lagasse <benedicte.lagasse@proton.me>
2024-10-24 16:08:37 +0200

    retrieved info and returned divinfo for each data.projets

--- src/Components/DivInfo/DivInfo.jsx
+++ src/Components/DivInfo/DivInfo.jsx
@@ -1,31 +1,44 @@
-import { useRef, useState } from 'react';
-//todo : récupérer le bouton
-const clickChecked = localStorage.getItem('clickCount');
-console.log('clickChecked : ' + clickChecked);
-
-//todo : faire un tableau des divinfo
-
+import { useEffect, useRef, useState } from 'react';
+import data from '../../../GAME/propositions.json';
+import styles from './DivInfo.module.css';
 const DivInfo = () => {
-  //todone == display divinfos
-  const displayInfodiv = useRef(false);
-  function handleClick() {
-    displayInfodiv.current = !displayInfodiv.current;
-    console.log('button clicked : ' + displayInfodiv.current);
-  }
+  //todo : récupérer les infos du json
+  const projects = data.projets;
+
+  //todo : récupérer les infos du bouton dans le local storage
+  // const clickChecked = localStorage.getItem('clickCount');
+  // console.log('clickChecked' + clickChecked);
+
+  // // const [clickCount, setClickCount] = useState(clickChecked);
+
+  // function displayDiv() {
+  //   localStorage.getItem('clickCount');
+  //   setClickCount(clickCount + 1);
+  // }
+
+  //   //todone == display divinfos
+  //   const displayInfodiv = useRef(false);
+  //   function handleClick() {
+  //     displayInfodiv.current = !displayInfodiv.current;
+  //     console.log('button clicked : ' + displayInfodiv.current);
+  //   }
+  // //todo : faire un tableau des divinfo
+  console.log(projects);
   return (
     <>
-      <button onClick={handleClick}>CLICK !!!</button>
-      <section>
-        <div>
-          <p>Infos 1ère question</p>
-        </div>
-        <div>
-          <p>Infos 2ème question</p>
-        </div>
-        <div>
-          <p>Infos 3ème question</p>
-        </div>
-      </section>
+      {projects.map((projet, index) => {
+        return (
+          <div key={'projet' + index} className={styles.infodiv}>
+            <h3 className={styles.infotitle}>
+              Read more about "{projet.title}"
+            </h3>
+            <p>{projet.information}</p>
+            <a href={projet.link}>
+              Source: Institute for Sustainable IT UE 2021
+            </a>
+          </div>
+        );
+      })}
     </>
   );
 };
